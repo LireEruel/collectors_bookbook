86a639dcebd63a5b889632b6f5c20ba03b0c90a2
LireEruel <jsh001505@gmail.com>
2022-12-31 15:01:37 +0900

    검색 UI 추가

--- app/main.py
+++ app/main.py
@@ -11,5 +11,13 @@ templates = Jinja2Templates(directory=BASE_DIR / "templates")
 @app.get("/items/{id}", response_class=HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse(
-        "item.html", {"request": request, "id": id, "data": "hello fast api"}
+        "item.html", {"request": request, "title": "콜렉터 북북이"}
+    )
+
+
+@app.get("/search", response_class=HTMLResponse)
+async def search(request: Request, q: str):
+    print(q)
+    return templates.TemplateResponse(
+        "item.html", {"request": request, "title": "콜렉터 북북이", "keyword": q}
     )
